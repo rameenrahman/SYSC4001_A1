827f3fb10c16cb8f40864aebe3c6a9e2fc3cda68
LindsayXu123 <bufferfly137@gmail.com>
2025-10-02 01:12:32 -0400

    added some more simulation code

--- interrupts.cpp
+++ interrupts.cpp
@@ -34,7 +34,53 @@ int main(int argc, char** argv) {
             execution += std::to_string(ms) + ", " + std::to_string(duration_intr) + ", CPU burst\n";
             ms += duration_intr;
         }
-        
+
+        else if (activity == "SYSCALL"){
+            const int device = duration_intr;
+            auto [ex, curr_time] = intr_boilerplate(ms, device, SAVE_TIME, vectors);
+            execution += ex;
+            ms = curr_time;
+
+            int time_left = delays[device];
+            while (time_left > 0){
+                int time_interval;
+                if (time_left >= 40){
+                    time_interval = 40;
+                }
+                else {
+                    time_interval = time_left;
+                }
+                execution += std::to_string(ms) + ", " + std::to_string(time_interval) + ", " + "call device driver" + "\n";
+                ms += time_interval;
+                time_left -= time_interval;
+            }
+
+            execution += std::to_string(ms) + ", 1, IRET\n";
+            ms += 1;
+        }
+        else if (activity == "END_IO"){
+            const int device = duration_intr;
+            auto [ex, curr_time] = intr_boilerplate(ms, device, SAVE_TIME, vectors);
+            execution += ex;
+            ms = curr_time;
+
+            int time_left = delays[device];
+            while (time_left > 0){
+                int time_interval;
+                if (time_left >= 40){
+                    time_interval = 40;
+                }
+                else {
+                    time_interval = time_left;
+                }
+                execution += std::to_string(ms) + ", " + std::to_string(time_interval) + ", " + "Store information in memory" + "\n";
+                ms += time_interval;
+                time_left -= time_interval;
+            }
+            execution += std::to_string(ms) + ", 1, IRET\n";
+            ms += 1;
+            
+        }
 
         /************************************************************************/
 
